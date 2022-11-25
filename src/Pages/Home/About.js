4ef80f4b3b1b1e0c5ff98216dc485c2ba4ae6794
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-25 12:42:11 +0600

    extra section and footer added

--- src/Pages/Home/About.js
+++ src/Pages/Home/About.js
@@ -1,7 +1,31 @@
 import React from "react";
+import truck5 from "../../assets/bannerImg/truck5.jpg";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={truck5}
+            className="lg:w-1/2 sm:w-3/4 rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">About Us!</h1>
+            <p className="py-6">
+              For 80 years, Peterbilt has supplied the used commercial vehicle
+              market with the industry’s most rugged, reliable and efficient
+              products. Based in use, Peterbilt brings on highway, vocational
+              and medium duty trucks that provide value to their owners and
+              pride to their drivers. These vehicles are supported through
+              industry-leading aftermarket programs and a dedicated network of
+              independent market.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
