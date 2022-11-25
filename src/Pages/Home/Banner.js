12882d98c3ab9535192924109625c8747a8ee055
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-25 12:32:23 +0600

    banner added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,61 @@
+import React from "react";
+import truck1 from "../../assets/bannerImg/truck1.jpg";
+import truck2 from "../../assets/bannerImg/truck2.jpg";
+import truck3 from "../../assets/bannerImg/truck3.png";
+import truck4 from "../../assets/bannerImg/truck4.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="carousel  ">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img alt="" src={truck1} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img alt="" src={truck2} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img alt="" src={truck3} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img alt="" src={truck4} className="w-full" />
+
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
