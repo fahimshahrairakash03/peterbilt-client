05d8e57a4c83007d6e045e1a5d22ac2d1d3f47bd
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-26 12:55:37 +0600

    booking confirmed

--- src/Pages/Category/BookingModal.js
+++ src/Pages/Category/BookingModal.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import toast from "react-hot-toast";
 import { AuthContext } from "../../context/AuthProvider";
 
 const BookingModal = ({ product }) => {
@@ -23,6 +24,21 @@ const BookingModal = ({ product }) => {
       location,
     };
     console.log(booking);
+
+    fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          toast.success("Booking confirmed");
+        }
+        console.log(data);
+      });
   };
 
   return (
