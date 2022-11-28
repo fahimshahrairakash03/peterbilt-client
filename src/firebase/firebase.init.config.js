598921733101332cac975b8e3cb965275ad36ba9
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-28 13:49:08 +0600

    final add

--- src/firebase/firebase.init.config.js
+++ src/firebase/firebase.init.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA48zR7XwyqhlPIvuEZflEvZuDSbp9OPJU",
-  authDomain: "peterbilt-8dc71.firebaseapp.com",
-  projectId: "peterbilt-8dc71",
-  storageBucket: "peterbilt-8dc71.appspot.com",
-  messagingSenderId: "598287863406",
-  appId: "1:598287863406:web:0ca0e2141df4c602b1607f",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
